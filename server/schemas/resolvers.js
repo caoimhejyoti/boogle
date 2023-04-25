27151ab4d05adaae2a1f2f8818508bd3644724c8
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-25 17:46:04 +0800

    adding resolvers base code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,11 +3,62 @@ const { User, Book } = require("./../models");
 const { signToken } = require("./../utils/auth");
 
 const resolvers = {
-    Query: {
-
+  Query: {
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate("savedBooks");
     },
+  },
 
-    Mutation: {
-
+  Mutation: {
+    creatUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
-}
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect Credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { userId, bookId }, context) => {
+      if (context.user) {
+        return User.findByOneAndUpdate(
+          { _id: user._id },
+          { $addToSet: { savedBooks: body } },
+          { new: true, runValidators: true }
+        );
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    deleteBook: async (parent, { userId, bookId }, context) => {
+      if (context.user) {
+        return Book.findByOneAndUpdate(
+          { _id: userId },
+          {
+            $pull: {
+              savedBooks: {
+                _id: bookId,
+              },
+            },
+          },
+          { new: true }
+        );
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+};
+
+module.exports = resolvers;
