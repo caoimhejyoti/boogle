5c68c50497bb801c7c8db665ae1a2aa7e7da2f0e
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-27 18:11:23 +0800

    updating all resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,8 +4,11 @@ const { signToken } = require("./../utils/auth");
 
 const resolvers = {
   Query: {
-    user: async (parent, { username }) => {
-      return User.findOne({ username }).populate("savedBooks");
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
 
@@ -32,29 +35,25 @@ const resolvers = {
 
       return { token, user };
     },
-    saveBook: async (parent, { userId, bookId }, context) => {
+    saveBook: async (parent, { book }, context) => {
       if (context.user) {
-        return User.findByOneAndUpdate(
-          { _id: user._id },
-          { $addToSet: { savedBooks: body } },
-          { new: true, runValidators: true }
+        const updatedUser = User.findByOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: book } },
+          { new: true}
         );
+        return updatedUser;
       }
       throw new AuthenticationError("You need to be logged in!");
     },
-    deleteBook: async (parent, { userId, bookId }, context) => {
+    deleteBook: async (parent, { bookId }, context) => {
       if (context.user) {
-        return Book.findByOneAndUpdate(
-          { _id: userId },
-          {
-            $pull: {
-              savedBooks: {
-                _id: bookId,
-              },
-            },
-          },
+        const updatedUser = User.findByOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: { bookId: bookId } } },
           { new: true }
         );
+        return updatedUser;
       }
       throw new AuthenticationError("You need to be logged in!");
     },
